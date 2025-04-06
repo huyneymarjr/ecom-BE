b397f62affa076c20d795c5f52856a6b0445d9a9
huyneymarjr <quanghuy18082003@gmail.com>
2025-04-06 11:01:03 +0700

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,61 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { SharedModule } from 'src/shared/shared.module'
+import { AuthModule } from 'src/routes/auth/auth.module'
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core'
+import CustomZodValidationPipe from 'src/shared/pipes/custom-zod-validation.pipe'
+import { ZodSerializerInterceptor } from 'nestjs-zod'
+import { HttpExceptionFilter } from 'src/shared/filters/http-exception.filter'
+import { LanguageModule } from 'src/routes/language/language.module'
+import { PermissionModule } from 'src/routes/permission/permission.module'
+import { RoleModule } from 'src/routes/role/role.module'
+import { ProfileModule } from 'src/routes/profile/profile.module'
+import { UserModule } from 'src/routes/user/user.module'
+import { MediaModule } from 'src/routes/media/media.module'
+import { BrandModule } from 'src/routes/brand/brand.module'
+import { BrandTranslationModule } from 'src/routes/brand/brand-translation/brand-translation.module'
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n'
+import path from 'path'
+import { CategoryModule } from 'src/routes/category/category.module'
+import { CategoryTranslationModule } from 'src/routes/category/category-translation/category-translation.module'
+
+@Module({
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.resolve('src/i18n/'),
+        watch: true,
+      },
+      resolvers: [{ use: QueryResolver, options: ['lang'] }, AcceptLanguageResolver],
+      typesOutputPath: path.resolve('src/generated/i18n.generated.ts'),
+    }),
+    SharedModule,
+    AuthModule,
+    LanguageModule,
+    PermissionModule,
+    RoleModule,
+    ProfileModule,
+    UserModule,
+    MediaModule,
+    BrandModule,
+    BrandTranslationModule,
+    CategoryModule,
+    CategoryTranslationModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: CustomZodValidationPipe,
+    },
+    { provide: APP_INTERCEPTOR, useClass: ZodSerializerInterceptor },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
+})
+export class AppModule {}
---
+++ src/shared/repositories/shared-user.repo.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common'
+import { PermissionType } from 'src/shared/models/shared-permission.model'
+import { RoleType } from 'src/shared/models/shared-role.model'
+import { UserType } from 'src/shared/models/shared-user.model'
+import { PrismaService } from 'src/shared/services/prisma.service'
+
+type UserIncludeRolePermissionsType = UserType & { role: RoleType & { permissions: PermissionType[] } }
+
+export type WhereUniqueUserType = { id: number } | { email: string }
+
+@Injectable()
+export class SharedUserRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  findUnique(where: WhereUniqueUserType): Promise<UserType | null> {
+    return this.prismaService.user.findFirst({
+      where: {
+        ...where,
+        deletedAt: null,
+      },
+    })
+  }
+
+  findUniqueIncludeRolePermissions(where: WhereUniqueUserType): Promise<UserIncludeRolePermissionsType | null> {
+    return this.prismaService.user.findFirst({
+      where: {
+        ...where,
+        deletedAt: null,
+      },
+      include: {
+        role: {
+          include: {
+            permissions: {
+              where: {
+                deletedAt: null,
+              },
+            },
+          },
+        },
+      },
+    })
+  }
+
+  update(where: { id: number }, data: Partial<UserType>): Promise<UserType | null> {
+    return this.prismaService.user.update({
+      where: {
+        ...where,
+        deletedAt: null,
+      },
+      data,
+    })
+  }
+}
